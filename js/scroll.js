10fe06081fb824f03373955c558f67a6f6e7cf8d
estherkuforiji <esther.kuforiji@gmail.com>
2016-06-08 17:11:19 +0100

    Added Get Involved section and amended ordered list styling

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,17 @@
     $("document").ready(function(){
         $('#button1').click(function(){
             $('html, body').animate({
-                scrollTop: $("#the-project").offset().top-300
+                scrollTop: $("#the-project").offset().top-200
             }, 2000);
         });
         $('#button2').click(function(){
             $('html, body').animate({
-                scrollTop: $("#the-team").offset().top-300
+                scrollTop: $("#the-team").offset().top-200
             }, 2000);
         });
         $('#button3').click(function(){
             $('html, body').animate({
-                scrollTop: $("#get-involved").offset().top-300
+                scrollTop: $("#get-involved").offset().top-200
             }, 112);
         });
     });
